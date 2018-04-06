235dab5cefc69708e99914492b63c211c68a7441
tianlinhe <tinaho_ok@hotmail.com>
2018-04-06 16:01:32 +0200

    Update sigmoidal.py

--- python/sigmoidal.py
+++ python/sigmoidal.py
@@ -7,7 +7,6 @@ def f(value):
     return((-1)*math.log(value,2))
 
 def one_d_dot(input):
-
     # open the excelfile
     xlsx = pd.ExcelFile(input)
 # get the first sheet=master proteins as an object
@@ -20,24 +19,15 @@ def one_d_dot(input):
     x = f1(foldchange)
     df['LOG2 (L/H)']=x
     
-    p = np.linspace(1,0,384)
+#split dots in 8 color categories according to their x-value
+#value of p makes it a sigmoidal shape. 
+    p = np.linspace(1,0,len(x))
     k=p
-    
-   
-    
-    separator = np.array([-6,-4,-2,-0,2,4,6,8])
     x_color = []
     k_color = []
+    separator = np.array([-6,-4,-2,-0,2,4,6,8])
     
-    #x_color = np.array(x,[],[],[],[],[],[],[])
-    alist = []
-    blist = [0]
-    blist = blist + alist
-    
-    
-    #print (separator)
     for number in separator:
-        print (number)
         count =0
         
         for item in x:
@@ -50,51 +40,26 @@ def one_d_dot(input):
         x=x2[1]
         k2=np.split(k,[count,len(k)])
         k_color.append(k2[0])
-        k=k2[1]
-        #print (x)
-        #print (x_color)
-        
-        #print (x2)
-    #print (alist)
-    #print (k_color[4])
-    
-    #print (len(x_color))
-    
+        k=k2[1]       
 
-
-
-    
-    colors = ['darkblue','cornflowerblue','lightskyblue','powderblue','mistyrose','lightcoral','firebrick','maroon']
-    y = np.random.random(size=len(foldchange))
-    #print (y)
-    #k = np.linspace(1,0,384)
-    #print (k)
+#plot the dots with 8 colors   
     fh, ax = plt.subplots(1,1)
+    colors = ['darkblue','cornflowerblue','lightskyblue','powderblue','mistyrose','lightcoral','firebrick','maroon']
+    
     for i in range(0,len(x_color)):
-        #k = np.random.random(size=len(x_color[i]))
         ax.scatter(x_color[i],k_color[i],color=colors[i])
+        
+ #add annotations for proteins in dic(indicators)       
     indicators = {'P50747':'Biotin ligase','Q99873':'PRMT1','P17181':'IFNAR1','P42224':'STAT1'}
-    #x_coor = []
-    #y_coor = []
-    print (len(y))
     for item in indicators:
         for row_num in range(len(foldchange)):
             if item == df.loc[:,'Accession'][row_num]:
-                print (item)
-                #y_coor.append(row_num)
-                #x_coor.append(df.loc[:,'LOG2 (L/H)'][row_num])
-                print (y[row_num])
                 ax.annotate(indicators[item],(df.loc[:,'LOG2 (L/H)'][row_num],p[row_num]),arrowprops=dict(facecolor='black', shrink=0.05))
                 break
-    #print (x_coor)
-    #print (y_coor)
-                
-                
+                               
     #ax.axes.get_yaxis().set_visible(False)
     plt.ylabel('Fraction')
     plt.xlabel('LOG2 (L/H)')
     plt.show()
-    
-    
-    
+      
 one_d_dot("20180319_04_Qp1_Nyberg_beads.xlsx")
